94f27b3f86d4a35596cd688dfc6c9d0ee920637a
CTN <info@ctn-originals.com>
2022-04-04 14:13:10 +0200

    small changes to contact panel

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -1,8 +1,13 @@
 
 
-function loadPage(target) {
+function loadPage(target, blank = false) {
     try {
-        window.location.href = '../' + target;
+        if (blank) {
+            window.open(target, '_blank').focus();
+        }
+        else {
+            window.location.href = '../' + target;
+        }
     } catch (error) {
         console.error('Page could not be loaded: \n' + target)
     }
